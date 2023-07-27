673438fb1859b37c26bd3727275898eb3a6ff2b9
PRATAP-KUMAR <pratap@fastmail.fm>
2023-07-27 20:57:29 +0530

    API changed from local to hosting

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -1,5 +1,5 @@
 // const apiUrl = 'https://gretagrundihoiuala.000webhostapp.com/productapp/api';
-const apiUrl = 'http://localhost/productapp/api';
-// const apiUrl = 'https://pratapkumar.me/productapp/api';
+// const apiUrl = 'http://localhost/productapp/api';
+const apiUrl = 'https://pratapkumar.me/productapp/api';
 
 export default apiUrl;
